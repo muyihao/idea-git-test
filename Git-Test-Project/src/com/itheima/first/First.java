829b32d18120f1e14641c0845d4a7424e10a081b
yanghao <yanghaocdut@foxmail.com>
2020-06-26 00:00:34 +0800

    新增几个输出语句

--- Git-Test-Project/src/com/itheima/first/First.java
+++ Git-Test-Project/src/com/itheima/first/First.java
@@ -3,5 +3,8 @@ package com.itheima.first;
 public class First {
     public static void main(String[] args) {
         System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
     }
 }
